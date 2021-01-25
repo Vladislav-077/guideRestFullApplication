06dca2484dc613c4307775fc851999351f5d01aa
Vladislav Abovyan <vladislav.abovian@aisa.ru>
2021-01-25 10:21:08 +0300

    init

---
+++ src/main/java/com/example/guides/GuidesRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.example.guides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidesRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuidesRestFullApplication.class, args);
+	}
+
+}
